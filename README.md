f98da07caa3829c143feeaad68a1d464fffb31ed
Yusinto Ngadiman <yusinto@gmail.com>
2016-10-21 22:11:16 +1100

    Added a single index.js file

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # universal-hot-reload
-Hot reload client and server webpack bundles for the ultimate development experience
+
+[![npm version](https://img.shields.io/npm/v/universal-hot-reload.svg?style=flat-square)](https://www.npmjs.com/package/universal-hot-reload) [![npm downloads](https://img.shields.io/npm/dm/universal-hot-reload.svg?style=flat-square)](https://www.npmjs.com/package/universal-hot-reload) [![npm](https://img.shields.io/npm/dt/universal-hot-reload.svg?style=flat-square)](https://www.npmjs.com/package/universal-hot-reload) [![npm](https://img.shields.io/npm/l/universal-hot-reload.svg?style=flat-square)](https://www.npmjs.com/package/universal-hot-reload) 
+
+> **Hot reload server and client webpack bundles for the ultimate development experience** :clap:
+
+<b>FOR DEVELOPMENT USE ONLY!</b>
+
+If you use webpack to bundle your app <b>BOTH</b> on the server <b>AND</b> the client side, this package will set up hot reloading for both the server and the client side.  
+
+What you get from this package:
+
+ * Automatic re-bundle of server app on server changes
+ * Automatic re-bundle of client app on client changes using webpack-dev-server and webpack HotModuleReplacementPlugin
+
+## Installation
+
+npm i --save universal-hot-reload
+
+## Quickstart
+
+1. In your server bootstrap, require universal-hot-reload and invoke it in development, passing it your server and client webpack config in that order. I use the config npm package to store all my app config:
+
+    ```javascript
+    require('babel-polyfill');
+    const config = require('config');
+    const universalHotReload = require('universal-hot-reload');
+    
+    if (config.appEnv === 'development') {
+      universalHotReload('path/to/webpack.server.config.js', 'path/to/webpack.client.config.js');
+    } else {
+      // PROD
+      require('/path/to/serverBundle.js'); // eslint-disable-line import/no-unresolved
+    }
+
+    ```
+
+2. In your server entry file (as specified in your webpack server config "entry" property), you need to export your express app (in the form of a httpServer object):
+
+    ```javascript
+    import config from 'config';
+    import express from 'express';
+    
+    const app = express();
+    app.use('/dist', express.static('dist', {maxAge: '1d'}));
+
+    // ...your other express middleware
+    
+    const httpServer = app.listen(config.port, () => {
+      log.info(`Listening at ${config.port}`);
+    });
+    
+    // export httpServer object so universal-hot-reload can access it
+    if(config.appEnv === 'development') {
+      module.exports = httpServer;
+    }
+
+    ```
+
+3. Run your app:
+    
+    ```javascript
+    node path/to/serverBootstrap.js
+    ```
+
+## Example
+Coming soon 
--- index.js
+++ index.js
@@ -1,3 +1,113 @@
-/**
- * Created by yus on 21/10/2016.
- */
+module.exports = function main(serverConfigPath, clientConfigPath, devServerPort) {
+  const webpack = require('webpack');
+  const serverConfig = require(serverConfigPath);
+  const clientConfig = require(clientConfigPath);
+
+  if (typeof devServerPort === 'undefined' || !devServerPort) {
+    devServerPort = 8001;
+  }
+
+  // Watch changes on the server side, re-compile and restart.
+  watchServerChanges(serverConfig);
+
+  // Start webpack dev server separately on a different port to avoid issues with httpServer restarts
+  watchClientChanges(clientConfig);
+
+  /**
+   * Clear bundled server cache
+   * @param serverBundlePath Path to the server bundle file.
+   */
+  function clearCache(serverBundlePath) {
+    Object.keys(require.cache).forEach(function (id) {
+      if (id === serverBundlePath) {
+        delete require.cache[id];
+      }
+    });
+  }
+
+  /**
+   * Starts express, caches and deletes sockets. Returns the httpServer object and all sockets in a Map.
+   * @param serverBundlePath Path to the server bundle file.
+   * @returns {{httpServer: *, sockets: Map}}
+   */
+  function initHttpServer(serverBundlePath) {
+    const httpServer = require(serverBundlePath);
+    const sockets = new Map();
+    let nextSocketId = 0;
+
+    // http://stackoverflow.com/questions/14626636/how-do-i-shutdown-a-node-js-https-server-immediately
+    httpServer.on('connection', function (socket) {
+      var socketId = nextSocketId++;
+      sockets.set(socketId, socket);
+
+      socket.on('close', function () {
+        sockets.delete(socketId);
+      });
+
+      socket.setTimeout(0);
+    });
+
+    return {httpServer, sockets};
+  }
+
+  /**
+   * Watches server for changes, recompile and restart express
+   */
+  function watchServerChanges(serverConfig) {
+    let initialLoad = true;
+    let httpServerInitObject; // contains the httpServer itself and sockets
+
+    const bundlePath = serverConfig.output.path + '/' + serverConfig.output.filename;
+    const serverCompiler = webpack(serverConfig);
+
+    // compile server side code
+    serverCompiler.watch({
+      aggregateTimeout: 300, // wait so long for more changes
+      poll: true // use polling instead of native watchers
+    }, function (err, stats) {
+      if (err) {
+        console.log('Server bundling error:' + JSON.stringify(err));
+        return;
+      }
+
+      clearCache(bundlePath);
+
+      if (!initialLoad) {
+        httpServerInitObject.httpServer.close(function () {
+          httpServerInitObject = initHttpServer(bundlePath);
+          console.log('Server restarted');
+        });
+
+        // Destroy all open sockets
+        for (var socket of httpServerInitObject.sockets.values()) {
+          socket.destroy();
+        }
+      } else {
+        initialLoad = false;
+        httpServerInitObject = initHttpServer(bundlePath);
+        console.log('Server bundling done');
+      }
+    });
+  }
+
+  /**
+   * Start webpack dev server for hmr
+   */
+  function watchClientChanges(clientConfig) {
+    const WebpackDevServer = require('webpack-dev-server');
+    const serverOptions = {
+      quiet: false, // don’t output anything to the console.
+      noInfo: true, // suppress boring information
+      hot: true, // switch the server to hot mode.
+      inline: true, // embed th
+      // e webpack-dev-server runtime into the bundle.
+      lazy: false, // no watching, compiles on request
+      contentBase: clientConfig.output.publicPath, // base path for the content.
+      publicPath: clientConfig.output.publicPath, // `http://localhost:8001/dist`
+      stats: true,
+    };
+    const devCompiler = webpack(clientConfig);
+    const devServer = new WebpackDevServer(devCompiler, serverOptions);
+    devServer.listen(devServerPort, 'localhost', console.log(`weback-dev-server listening at ${devServerPort}`));
+  }
+};
