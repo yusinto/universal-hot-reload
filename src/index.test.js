20b41cc0493cf1d0ee3441843f47f881768431e3
Yusinto Ngadiman <yus@launchdarkly.com>
2018-11-18 15:06:55 +1100

    Added client watch tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,13 +1,16 @@
 jest.mock('webpack', () => jest.fn());
+jest.mock('webpack-dev-server', () => jest.fn());
 jest.mock('./clearRequireCache', () => jest.fn());
 jest.mock('./initHttpServer', () => jest.fn());
 
 import webpack from 'webpack';
+import webpackDevServer from 'webpack-dev-server';
 import clearRequireCache from './clearRequireCache';
-import { getDevServerBundleUrl, watchServerChanges } from './index';
+import { getDevServerBundleUrl, watchServerChanges, watchClientChanges } from './index';
 import initHttpServer from './initHttpServer';
 
-const clientConfig = {
+let clientConfig = {
+  entry: './app-entry.js',
   output: {
     publicPath: 'http://localhost:8001/dist/',
     filename: 'bundle.js',
@@ -22,14 +25,6 @@ const serverConfig = {
 };
 
 describe('index.js', () => {
-  let mockServerCompiler;
-  beforeEach(() => {
-    mockServerCompiler = {
-      watch: jest.fn(),
-    };
-    webpack.mockImplementation(() => mockServerCompiler);
-  });
-
   afterEach(() => {
     jest.resetAllMocks();
   });
@@ -39,8 +34,89 @@ describe('index.js', () => {
     expect(result).toEqual('http://localhost:8001/dist/bundle.js');
   });
 
-  describe('server', () => {
+  describe('client', () => {
+    let mockClientCompiler;
+    let mockHmrPlugin;
+    let wdsMockInstance;
+
     beforeEach(() => {
+      mockClientCompiler = {
+        watch: jest.fn(),
+      };
+      mockHmrPlugin = jest.fn();
+      wdsMockInstance = {
+        listen: jest.fn(),
+      };
+      webpack.mockImplementation(() => mockClientCompiler);
+      webpack.HotModuleReplacementPlugin = () => mockHmrPlugin;
+      webpackDevServer.mockImplementation(() => wdsMockInstance);
+    });
+
+    test('hmr entries and plugin are added to client config', () => {
+      watchClientChanges({ ...clientConfig });
+
+      const { entry, output, plugins } = webpack.mock.calls[0][0];
+      expect(entry.length).toEqual(3);
+      expect(entry[0]).toEqual(clientConfig.entry);
+      expect(entry[1]).toEqual(expect.stringContaining('webpack-dev-server/client/index.js?http://localhost:8001'));
+      expect(entry[2]).toEqual(expect.stringContaining('webpack/hot/dev-server.js'));
+      expect(output).toEqual(clientConfig.output);
+      expect(plugins).toEqual([mockHmrPlugin]);
+    });
+
+    test('hmr entries and plugin are appended to client config', () => {
+      const clientConfigClone = { ...clientConfig, entry: ['./app1.js', './app2.js'], plugins: ['some-other-plugin'] };
+
+      watchClientChanges(clientConfigClone);
+
+      const { entry, output, plugins } = webpack.mock.calls[0][0];
+      expect(entry.length).toEqual(4);
+      expect(entry[0]).toEqual('./app1.js');
+      expect(entry[1]).toEqual('./app2.js');
+      expect(entry[2]).toEqual(expect.stringContaining('webpack-dev-server/client/index.js?http://localhost:8001'));
+      expect(entry[3]).toEqual(expect.stringContaining('webpack/hot/dev-server.js'));
+      expect(output).toEqual(clientConfig.output);
+      expect(plugins).toEqual(['some-other-plugin', mockHmrPlugin]);
+    });
+
+    test('webpack-dev-server constructed with correct options', () => {
+      watchClientChanges({ ...clientConfig });
+
+      const options = webpackDevServer.mock.calls[0][1];
+
+      expect(webpackDevServer).toBeCalledTimes(1);
+      expect(options).toEqual({
+        quiet: false,
+        noInfo: false,
+        lazy: false,
+        publicPath: 'http://localhost:8001/dist/',
+        stats: 'errors-only',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        hot: true,
+      });
+    });
+
+    test('listen gets called with the right port', () => {
+      watchClientChanges({ ...clientConfig });
+
+      const listenCall = wdsMockInstance.listen.mock.calls[0];
+      expect(wdsMockInstance.listen).toBeCalledTimes(1);
+      expect(listenCall[0]).toEqual('8001');
+      expect(listenCall[1]).toEqual('localhost');
+      expect(typeof listenCall[2]).toEqual('function');
+    });
+  });
+
+  describe('server', () => {
+    let mockServerCompiler;
+
+    beforeEach(() => {
+      mockServerCompiler = {
+        watch: jest.fn(),
+      };
+      webpack.mockImplementation(() => mockServerCompiler);
       watchServerChanges(serverConfig);
     });
 
